16e47ab1f313b8c710d2956f3e0a3d84cc8c995c
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-03-24 16:15:01 -0400

    add fake JobApplication data and display some properties

--- job-app-tracker/src/app/home/home.component.ts
+++ job-app-tracker/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { JobApplication } from '../common/types/JobApplication';
 
 @Component({
   selector: 'app-home',
@@ -11,5 +12,46 @@ import { RouterOutlet } from '@angular/router';
 })
 
 export class HomeComponent {
-  jobApplications = ["Application 1", "Application 2", "Application 3", "Application 4", "Application 5"]
+  jobApplications : JobApplication[] = []
+
+  /**
+   * Set job applications field
+   */
+  constructor() {
+    this.jobApplications = [
+      {
+        company: "Ubisoft",
+        position: "Junior Dev",
+        location: "Montreal",
+        employmentType: "Full-time",
+        commute: "On-site",
+        stage: ["1", "2"],
+        status: "Not Applied",
+        dateApplied: new Date(),
+        updatedOn: new Date(),
+      },
+      {
+        company: "Ubisoft",
+        position: "Junior Dev",
+        location: "Montreal",
+        employmentType: "Full-time",
+        commute: "On-site",
+        stage: ["1", "2"],
+        status: "Not Applied",
+        dateApplied: new Date(),
+        updatedOn: new Date(),
+      },
+      {
+        company: "Ubisoft",
+        position: "Junior Dev",
+        location: "Montreal",
+        employmentType: "Full-time",
+        commute: "On-site",
+        stage: ["1", "2"],
+        status: "Not Applied",
+        dateApplied: new Date(),
+        updatedOn: new Date(),
+      }
+    ]
+  }
 }
