d5ee3d5914c9b4a48175ece83c20362b9e169efd
Kyle Veloso <kyle.veloso@dawsoncollege.qc.ca>
2024-04-14 23:00:21 -0400

    add item to list upon submit with default values

--- job-app-tracker/src/app/common/types/jobApplication.ts
+++ job-app-tracker/src/app/common/types/jobApplication.ts
@@ -1,11 +1,16 @@
+export type EmploymentType = "Full-time" | "Part-time" | "Contract" | "Freelance" | "Internship"
+export type CommuteType = "On-site" | "Remote" | "Hybrid"
+export type ApplicationStatus = "Not Applied" | "Applied" | "Interviewing" | "Offer Received" | "Accepted Offer" | "Widthdrawn" | "Rejected"
+
+
 export interface JobApplication {
     company: string,
     position: string,
     location: string,
-    employmentType: "Full-time" | "Part-time" | "Contract" | "Freelance" | "Internship"
-    commute: "On-site" | "Remote" | "Hybrid",
+    employmentType: EmploymentType,
+    commute: CommuteType,
     stage: string[],
-    status: "Not Applied" | "Applied" | "Interviewing" | "Offer Received" | "Accepted Offer" | "Widthdrawn" | "Rejected"
-    dateApplied: Date, // i dont like dates,
+    status: ApplicationStatus
+    dateApplied: Date | undefined, // i dont like dates,
     updatedOn: Date
 }
